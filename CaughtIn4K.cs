34fd231c253a4b43dd99582a0ee2305608a32e8c
St4lky <91354095+St4lkyGhostface@users.noreply.github.com>
2021-09-24 20:12:25 -0500

    First Upload
    Accuse yourself instead.

---
+++ CaughtIn4K.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostCosmeticsFree
+{
+    // Getting the skidders in 8K, the German 13yo aka Impulse/Shock kid can't hide forever
+    class CaughtIn4K
+    {
+
+        public static string CaughtYou = "are ye stealing code son? just caught you in 8K lmao";
+
+    }
+}
